c1a08d317ab8a109979957f9fe577823a0011800
Alan P John <alanpjohn@outlook.com>
2024-09-08 09:35:19 +0100

    fix: character escape on home page

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function IndexPage() {
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Alan's NextJS Template <br className="hidden sm:inline" />
-          based on shadcn's template
+          Alan&lsquo;s NextJS Template <br className="hidden sm:inline" />
+          based on shadcn&lsquo;s template
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
           NextJS 14 Ready
